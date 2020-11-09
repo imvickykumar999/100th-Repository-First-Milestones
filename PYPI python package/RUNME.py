081783e2c25fc0bee04710d8f71fee28f9004552
Vicky Kumar <imvickykumar999@gmail.com>
2020-11-09 00:07:35 -0800

    Update RUNME.py

--- PYPI python package/RUNME.py
+++ PYPI python package/RUNME.py
@@ -1,8 +1,6 @@
 
 import os
 
-os.system('git clone https://github.com/imvickykumar999/100th-Repository-Morsetor-python-Package.git')
-
 os.system('pip install -r requirements.txt')
 
 os.system('python setup.py sdist')
