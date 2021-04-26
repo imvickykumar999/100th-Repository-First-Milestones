fcbc411129b736cbe4c3fadd8a7698943ac50939
imvickykumar999 <imvickykumar999@gmail.com>
2021-04-26 23:32:26 +0530

    firebase added

--- PYPI python package/setup.py
+++ PYPI python package/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'vixuploader', # while installing pacakge, change name to something unique on pypi.org
-    version = '0.0.2', # use different version if updated, like '0.0.2'
+    version = '0.0.6', # use different version if updated, like '0.0.2'
     description = 'Website : https://github.com/imvickykumar999/100th-Repository-Morsetor-python-Package',
     long_description = open('README.md').read(),
-    url = 'https://vixportfoliowithflask.herokuapp.com/skills',
+    url = 'https://imvickykumar999.herokuapp.com/news',
     author = 'Vicky Kumar',
-    keywords = ['PyPi Uploader', 'GitHub Uploader','Morse','custom','python package','function and class','3D line',
+    keywords = ['Firebase', 'GitHub Uploader','Morse','custom','python package','function and class','3D line',
     '3D plane', 'angle bw planes or line', 'distance bw point and plane'],
     license = 'MIT',
     packages = ['vixuploader'], # while importing package
